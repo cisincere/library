173b2b5400f9be1fb58364b0570419fbd46e652e
lbb <lbb>
2020-02-14 19:23:48 +0800

    完善urlConfig的配置

--- utils/urlConfig.js
+++ utils/urlConfig.js
@@ -1,13 +1,13 @@
 const serverRoot = 'http://176.47.8.88:8999'
 
 // 获取所有座位信息
-const chairs = serverRoot + '/chair'
+const chairs = serverRoot + '/chair/'
 
 // 获取轮播图
-const banner = serverRoot + '/banner'
+const banner = serverRoot + '/banner/'
 
 // 占座
-const chair_id = serverRoot+'/chair';
+const chair_id = serverRoot + '/chair/';
 
 // 搜索
 const search = serverRoot + '/search';
@@ -15,4 +15,6 @@ const search = serverRoot + '/search';
 module.exports = {
   chair_id: chair_id,
   search: search,
+  chairs: chairs,
+  banner: banner,
 }
